ddadf24aa4d072ecbb09040588bb9d02bb9537c0
Diaa-E <blarb21@yahoo.com>
2024-03-07 21:30:51 +0200

    Add a loading screen

--- src/ImageComponent.jsx
+++ src/ImageComponent.jsx
@@ -4,6 +4,7 @@ export default function ImageComponent({})
 {
     const [imageSrc, setImageSrc] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -14,15 +15,21 @@ export default function ImageComponent({})
 
             return response.json();
         })
-        .then(response => setImageSrc(response[Math.floor(Math.random() * response.length)].url))
+        .then(response => {
+
+            setImageSrc(response[Math.floor(Math.random() * response.length)].url);
+        })
         .catch(error => {
             console.error(error);
             setError(error);
-        });
+        })
+        .finally(() => setLoading(false));
 
     }, []);
 
     if (error) return <p>Error loading image</p>
+
+    if (loading) return <p>Loading...</p>
 
     return (
         imageSrc &&
