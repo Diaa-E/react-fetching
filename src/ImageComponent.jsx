be2d9922c41207444f6bcdfaa3d3338246365a81
Diaa-E <blarb21@yahoo.com>
2024-03-07 21:28:25 +0200

    Handle errors

--- src/ImageComponent.jsx
+++ src/ImageComponent.jsx
@@ -3,15 +3,26 @@ import { useEffect, useState } from "react"
 export default function ImageComponent({})
 {
     const [imageSrc, setImageSrc] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
 
         fetch("https://jsonplaceholder.typicode.com/photos", {mode: "cors"})
-        .then(response => response.json())
+        .then(response => {
+
+            if (response.status >= 400) throw new Error(`Error ${response.status}`)
+
+            return response.json();
+        })
         .then(response => setImageSrc(response[Math.floor(Math.random() * response.length)].url))
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.error(error);
+            setError(error);
+        });
 
     }, []);
+
+    if (error) return <p>Error loading image</p>
 
     return (
         imageSrc &&
